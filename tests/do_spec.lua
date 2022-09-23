e1145162b0d4cf3f84022536ac58b2311ed2cbec
shawn yu <shawn.yu5@hotmail.com>
2022-09-22 20:27:13 -0400

    chore: remove comments

--- tests/do_spec.lua
+++ tests/do_spec.lua
@@ -1,4 +1,5 @@
 local do_nvim = require("do")
+
 describe("Setup", function()
 	it("should set up default values if no user config is provided", function()
 		local state = require("do.state").state
@@ -9,9 +10,7 @@ describe("Setup", function()
 
 	it("should use custom config when setup is called", function()
 		local state = require("do.state").state
-		local default_opts = require("do.state").default_opts
 		local opts = {
-			-- default options
 			message_timeout = 2000, -- how long notifications are shown
 			kaomoji_mode = 0, -- 0 kaomoji everywhere, 1 skip kaomoji in doing
 			doing_prefix = "xxx: ",
@@ -30,4 +29,7 @@ describe("Setup", function()
 		assert.are.same(state.options.store.auto_create_file, opts.store.auto_create_file)
 		assert.are.same(state.options.store.file_name, opts.store.file_name)
 	end)
+end)
+
+describe(":Do", function()
 end)
