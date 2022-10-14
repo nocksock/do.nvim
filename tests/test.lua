257ed31843b01b0ed1c4bc8841d00864175e14cc
Nils Riedemann <nils@bleepbloop.studio>
2022-10-15 00:51:01 +0200

    tests: adjust auto running environment
    open the file in in split so winbar_nc can be seen.

--- tests/test.lua
+++ tests/test.lua
@@ -3,7 +3,12 @@ vim.wo.number = true
 vim.o.relativenumber = true
 vim.o.swapfile = false
 
+vim.keymap.set('n', '<c-j>', '<c-w>j')
+vim.keymap.set('n', '<c-k>', '<c-w>k')
+vim.keymap.set('n', '<c-l>', '<c-w>l')
+vim.keymap.set('n', '<c-h>', '<c-w>h')
+
 require("do").setup({
-  use_winbar = true
+  winbar = true
 })
 
