4922d5d05104b5c9a3f5827cb767822a104e1ccc
Nils Riedemann <nils@bleepbloop.studio>
2024-02-05 14:27:10 +0100

    feat: display task in selection

--- lua/do/action/select.lua
+++ lua/do/action/select.lua
@@ -1,7 +1,12 @@
 --- @type DoAction
-return function ()
+return function()
   return function(state, resolve)
-    vim.ui.select(state.todos, {
+    local available_todos = {}
+    for index, value in ipairs(state.todos) do
+      table.insert(available_todos, value.task)
+    end
+
+    vim.ui.select(available_todos, {
       prompt = "Select a todo",
     }, function(_, index)
       state.current_todo = index
