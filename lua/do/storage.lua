a03a1a28a945a7fb29e28e8cb11a79c52725a1ae
Nils Riedemann <nils@bleepbloop.studio>
2024-02-05 02:02:59 +0100

    chore: cleanup obsolete files

--- lua/do/storage.lua
+++
@@ -1,8 +0,0 @@
-local data = {}
-
-return {
-  add = function(item) table.insert(data, item) end,
-  get_all = function() return data end,
-  size = function() return #data end,
-  clear = function() data = {} end,
-}
--- scratch/mutliple-returns-into-fn.lua
+++
@@ -1,7 +0,0 @@
-local function foo() return 1, 2, 3 end
-
-local function other(a,b)
-  P({a = a, b = b})
-end
-
-other({foo()}, "foo")
