8694f658b2a4c0683f1067733f1eaba810d209d8
Nils Riedemann <nils@bleepbloop.studio>
2024-02-05 01:49:55 +0100

    feat: add .now action

---
+++ lua/do/action/now.lua
@@ -0,0 +1,22 @@
+---@type DoAction<string>
+return function(item)
+  return function(state)
+    if item == "" then
+      vim.ui.input({ prompt = 'Todo: ' }, function(input)
+        item = input
+      end)
+    end
+
+    if type(item) == "string" then
+      item = { task = item, done = false }
+    end
+
+    table.insert(state.todos, 1, item)
+
+    if not state.current_todo then
+      state.current_todo = 1
+    end
+
+    return state
+  end
+end
